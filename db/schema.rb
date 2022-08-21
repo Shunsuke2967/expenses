b0d60b978201df77988eb0ca1b928cc6e9e08d35
fukumura <shunshun2967@gmail.com>
2022-08-20 18:01:02 -0700

    デモユーザーかどうかのフラグ追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_14_132452) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_20_231127) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -90,6 +90,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_14_132452) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "demo", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
