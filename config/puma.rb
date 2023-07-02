957e9afdb0ee7ceec7a0c68e8375118b04becbed
fukumura <shunshun2967@gmail.com>
2023-07-01 19:45:30 -0700

    puma設定変更

--- config/puma.rb
+++ config/puma.rb
@@ -15,11 +15,11 @@ min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 
 plugin :tmp_restart
 threads min_threads_count, max_threads_count
-if !Rails.env.production?
-  port        ENV.fetch("PORT") { 3000 }
-  pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
-else
+if ENV['RAILS_ENV'] == 'production'
   app_root = File.expand_path("../..", __FILE__)
   bind "unix://#{app_root}/tmp/sockets/puma.sock"
   stdout_redirect "#{app_root}/log/puma.stdout.log", "#{app_root}/log/puma.stderr.log", true
+else
+  port        ENV.fetch("PORT") { 3000 }
+  pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 end
