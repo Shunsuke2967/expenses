b9f63884b6df28cbaae75ed9f48cf844483f3eb2
fukumura <shunshun2967@gmail.com>
2023-07-04 05:29:35 -0700

    view側のロジックをdecoratorに移行

--- app/models/template.rb
+++ app/models/template.rb
@@ -10,27 +10,4 @@ class Template < ApplicationRecord
                insurance: 5,
                payment: 6,
                other: 100 }
-
-  def set_class
-    case icon
-    when 'rent'
-      ['icon-color-red', 'fas fa-home']
-    when 'food_expenses'
-      ['icon-color-yellow', 'fas fa-utensils']
-    when 'cost_of_living'
-      ['icon-color-lightblue', 'fas fa-faucet']
-    when 'entertainment'
-      ['icon-color-green', 'fas fa-shopping-cart']
-    when 'car_cost'
-      ['icon-color-violetgreen', 'fas fa-car-side']
-    when 'insurance'
-      ['icon-color-violet', 'fas fa-hospital-user']
-    when 'payment'
-      ['icon-color-gray', 'fas fa-yen-sign']
-    when 'other'
-      ['icon-color-gray', 'far fa-question-circle']
-    else
-      ''
-    end
-  end
 end
